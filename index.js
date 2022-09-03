7ee163c969333db8f98a7bcb49e20dae29481f8f
Ms-Waiyego <waiyegobrenda99@gmail.com>
2022-09-03 19:19:16 +0300

    completed assignment

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ function fetchBooks() {
   return fetch("https://anapioficeandfire.com/api/books")
   .then((response)=> response.json())
   .then(books => renderBooks(books));
-  
 }
-
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
